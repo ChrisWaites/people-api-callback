a25bc837ad774fa5d27cf36639f503c7a9592754
Chris Waites <cwaites3@gatech.edu>
2018-05-01 20:19:26 -0400

    Fix app.

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
-def receive_message():
+def handler():
+    print(request.args)
     print(request.data)
+    print(request.text)
+    return 'Success!'
 
 if __name__ == "__main__":
     app.run()
